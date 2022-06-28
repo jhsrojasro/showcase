01a962f2088569663f31d47975f4a694875044ae
hamohseni <55771991+hamohseni@users.noreply.github.com>
2022-06-27 23:15:47 -0500

    Update photomosaicSoftware.js

--- content/sketches/photomosaicSoftware.js
+++ content/sketches/photomosaicSoftware.js
@@ -12,15 +12,15 @@ let blues = [];
 let greens = [];
 
 function preload() {
-  image_src = loadImage("/showcase/sketches/messi.jpg");
-  video_src = createVideo(["/showcase/sketches/messi-skill.mp4"]);
+  image_src = loadImage("/showcase/content/sketches/messi.jpg");
+  video_src = createVideo(["/showcase/content/sketches/messi-skill.mp4"]);
   video_src.hide(); // by default video shows up in separate dom
   photos = [];
   for (let i = 1; i <= 40; i++) {
     if (i.toString().length == 1) {
-      photos.push(loadImage(`/showcase/sketches/shaders/paintings2/00000${i}.jpg`));
+      photos.push(loadImage(`/showcase/content/sketches/shaders/paintings2/00000${i}.jpg`));
     } else {
-      photos.push(loadImage(`/showcase/sketches/shaders/paintings2/0000${i}.jpg`));
+      photos.push(loadImage(`/showcase/content/sketches/shaders/paintings2/0000${i}.jpg`));
     }
   }
 }
